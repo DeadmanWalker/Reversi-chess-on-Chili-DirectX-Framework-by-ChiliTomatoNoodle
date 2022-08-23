fedd6f8051ac274ecc495e45c481889b4ee1aa6b
The Fool <minerender255@gmail.com>
2022-08-23 22:43:56 +0700

    Added DrawPiece and hash related function to Board class

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -1,9 +1,46 @@
 #include "Board.h"
+#include "assert.h"
 
 void Board::DrawBoard(Graphics & gfx) const
 {
 	gfx.DrawRectDim(pos_x, pos_y, width * cell_dim, height * cell_dim, bg_color);
 	DrawCell(gfx);
+	DrawPiece(gfx);
+}
+
+Board::Board()
+{
+	for (int i = 0; i < width; ++i)
+	{
+		for (int j = 0; j < height; ++j)
+		{
+			cells[i][j] = 0;
+		}
+	}
+	cells[width / 2][height / 2] = 1;
+	cells[width / 2 - 1][height / 2 - 1] = 1;
+	cells[width / 2 - 1][height / 2] = 2;
+	cells[width / 2][height / 2 - 1] = 2;
+
+	cell_hash.push_back(HashFunc({ width / 2, height / 2 }, 1));
+	cell_hash.push_back(HashFunc({ width / 2 - 1, height / 2 - 1 }, 1));
+	cell_hash.push_back(HashFunc({ width / 2 - 1, height / 2 }, 2));
+	cell_hash.push_back(HashFunc({ width / 2, height / 2 - 1 }, 2));
+}
+
+void Board::ResetBoard()
+{
+	for (int i = 0; i < width; ++i)
+	{
+		for (int j = 0; j < height; ++j)
+		{
+			cells[i][j] = 0;
+		}
+	}
+	cells[width / 2][height / 2] = 1;
+	cells[width / 2 - 1][height / 2 - 1] = 1;
+	cells[width / 2 - 1][height / 2] = 2;
+	cells[width / 2][height / 2 - 1] = 2;
 }
 
 void Board::DrawCell(Graphics & gfx) const
@@ -17,4 +54,34 @@ void Board::DrawCell(Graphics & gfx) const
 				cell_dim - 2 * cell_offset, cell_dim - 2 * cell_offset, cell_color);
 		}
 	}
+}
+
+void Board::DrawPiece(Graphics & gfx) const
+{
+	int offset_x = pos_x + cell_dim / 2;
+	int offset_y = pos_y + cell_dim / 2;
+	for (int i = 0; i < width; ++i)
+	{
+		for (int j = 0; j < height; ++j)
+		{
+			if (cells[i][j] == 1)
+			{
+				gfx.DrawCircle(offset_x + i * cell_dim, offset_y + j * cell_dim, cell_dim / 2 - 5, Colors::White);
+			}
+			if (cells[i][j] == 2)
+			{
+				gfx.DrawCircle(offset_x + i * cell_dim, offset_y + j * cell_dim, cell_dim / 2 - 5, Colors::Black);
+			}
+		}
+	}
+}
+
+int Board::HashFunc(Location loc, int id)
+{
+	return (1 - 2 * (id == 2)) * (width * loc.y + loc.x + 1);
+}
+
+Location Board::DeHashFunc(int hash)
+{
+	return Location((abs(hash) - 1) % 8, (abs(hash) - 1) / 8);
 }
--- Engine/Board.h
+++ Engine/Board.h
@@ -1,15 +1,22 @@
 #pragma once
-#include"Location.h"
-#include"Graphics.h"
+#include "Location.h"
+#include "Graphics.h"
+#include "Mouse.h"
+#include <vector>
 
 
 class Board
 {
 public:
 	void DrawBoard(Graphics& gfx) const;
+	Board();
+	void ResetBoard();
 private:
 	void DrawCell(Graphics& gfx) const;
-
+	void DrawPiece(Graphics& gfx) const;
+	int HashFunc(Location loc, int id);
+	Location DeHashFunc(int hash);
+private:
 	static constexpr int height = 8;
 	static constexpr int width = 8;
 	static constexpr int cell_dim = 50;
@@ -17,4 +24,7 @@ private:
 	static constexpr int pos_y = 100;
 	static constexpr Color bg_color = Color(182, 125, 34);
 	static constexpr Color cell_color = Color(116, 71, 0);
+
+	int cells[width][height];
+	std::vector<int> cell_hash;
 };
