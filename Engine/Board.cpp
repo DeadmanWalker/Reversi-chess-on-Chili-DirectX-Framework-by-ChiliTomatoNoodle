12d99a393158b4cba370d98170d8972b41b5aa87
The Fool <minerender255@gmail.com>
2022-08-24 23:27:22 +0700

    add PlacePiece to Board and revise DrawPiece

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -5,7 +5,22 @@ void Board::DrawBoard(Graphics & gfx) const
 {
 	gfx.DrawRectDim(pos_x, pos_y, width * cell_dim, height * cell_dim, bg_color);
 	DrawCell(gfx);
-	DrawPiece(gfx);
+	
+	if (draw_ghost_piece)
+	{
+		DrawPiece(gfx, selected_cell, 2 - curr_turn % 2);
+	}
+	
+	for (int i = 0; i < width; ++i)
+	{
+		for (int j = 0; j < height; ++j)
+		{
+			if (cells[i][j] != 0)
+			{
+				DrawPiece(gfx, { i, j }, cells[i][j]);
+			}
+		}
+	}
 }
 
 Board::Board()
@@ -37,15 +52,43 @@ void Board::ResetBoard()
 			cells[i][j] = 0;
 		}
 	}
+
+	cell_hash.clear();
+
 	cells[width / 2][height / 2] = 1;
 	cells[width / 2 - 1][height / 2 - 1] = 1;
 	cells[width / 2 - 1][height / 2] = 2;
 	cells[width / 2][height / 2 - 1] = 2;
+
+	cell_hash.push_back(HashFunc({ width / 2, height / 2 }, 1));
+	cell_hash.push_back(HashFunc({ width / 2 - 1, height / 2 - 1 }, 1));
+	cell_hash.push_back(HashFunc({ width / 2 - 1, height / 2 }, 2));
+	cell_hash.push_back(HashFunc({ width / 2, height / 2 - 1 }, 2));
+}
+
+void Board::PlacePiece(const Mouse & mouse)
+{
+	draw_ghost_piece = false;
+	if (mouse.GetPosX() - pos_x >= 0 && (mouse.GetPosX() - pos_x) / cell_dim < width
+		&& mouse.GetPosY() - pos_y >= 0 && (mouse.GetPosY() - pos_y) / cell_dim < height
+		&& !(pre_mouse_input && mouse.LeftIsPressed()))
+	{
+		draw_ghost_piece = true;
+		int curr_player = 2 - curr_turn % 2;
+		selected_cell = { (mouse.GetPosX() - pos_x) / cell_dim, (mouse.GetPosY() - pos_y) / cell_dim };
+		if (!pre_mouse_input && mouse.LeftIsPressed() && cells[selected_cell.x][selected_cell.y] == 0)
+		{
+			cells[selected_cell.x][selected_cell.y] = curr_player;
+			cell_hash.push_back(HashFunc(selected_cell, curr_player));
+			++curr_turn;
+		}
+	}
+
+	pre_mouse_input = mouse.LeftIsPressed();
 }
 
 void Board::DrawCell(Graphics & gfx) const
 {
-	const int cell_offset = 2;
 	for (int i = 0; i < width; ++i)
 	{
 		for (int j = 0; j < height; ++j)
@@ -56,32 +99,19 @@ void Board::DrawCell(Graphics & gfx) const
 	}
 }
 
-void Board::DrawPiece(Graphics & gfx) const
+void Board::DrawPiece(Graphics & gfx, Location loc, int id) const
 {
 	int offset_x = pos_x + cell_dim / 2;
 	int offset_y = pos_y + cell_dim / 2;
-	for (int i = 0; i < width; ++i)
-	{
-		for (int j = 0; j < height; ++j)
-		{
-			if (cells[i][j] == 1)
-			{
-				gfx.DrawCircle(offset_x + i * cell_dim, offset_y + j * cell_dim, cell_dim / 2 - 5, Colors::White);
-			}
-			if (cells[i][j] == 2)
-			{
-				gfx.DrawCircle(offset_x + i * cell_dim, offset_y + j * cell_dim, cell_dim / 2 - 5, Colors::Black);
-			}
-		}
-	}
+	gfx.DrawCircle(offset_x + loc.x * cell_dim, offset_y + loc.y * cell_dim, cell_dim / 2 - 5, player_color[id - 1]);
 }
 
 int Board::HashFunc(Location loc, int id)
 {
-	return (1 - 2 * (id == 2)) * (width * loc.y + loc.x + 1);
+	return (1 - 2 * ((2 - curr_turn % 2) == 2)) * (width * loc.y + loc.x + 1);
 }
 
 Location Board::DeHashFunc(int hash)
 {
-	return Location((abs(hash) - 1) % 8, (abs(hash) - 1) / 8);
+	return Location((abs(hash) - 1) % width, (abs(hash) - 1) / width);
 }
--- Engine/Board.h
+++ Engine/Board.h
@@ -11,20 +11,27 @@ public:
 	void DrawBoard(Graphics& gfx) const;
 	Board();
 	void ResetBoard();
+	void PlacePiece(const Mouse& mouse);
 private:
 	void DrawCell(Graphics& gfx) const;
-	void DrawPiece(Graphics& gfx) const;
+	void DrawPiece(Graphics& gfx, Location loc, int id) const;
 	int HashFunc(Location loc, int id);
 	Location DeHashFunc(int hash);
 private:
 	static constexpr int height = 8;
 	static constexpr int width = 8;
 	static constexpr int cell_dim = 50;
+	static constexpr int cell_offset = 2;
 	static constexpr int pos_x = 200;
 	static constexpr int pos_y = 100;
 	static constexpr Color bg_color = Color(182, 125, 34);
 	static constexpr Color cell_color = Color(116, 71, 0);
+	static constexpr Color player_color[2] = { Colors::White, Colors::Black };
 
 	int cells[width][height];
 	std::vector<int> cell_hash;
+	Location selected_cell;
+	int curr_turn = 1;
+	bool pre_mouse_input = false;
+	bool draw_ghost_piece = true;
 };
